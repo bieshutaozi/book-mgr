eb0e3dcf9631345b0de38ccaf2a693a28d789dd8
bieshutaozi <1070048332@qq.com>
2021-03-19 22:23:45 +0800

    前端初始化界面

--- book-mgr-fe/src/router/index.js
+++ book-mgr-fe/src/router/index.js
@@ -1,24 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import Home from '../views/Home.vue';
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/auth',
+    name: 'Auth',
+    component: () => import(/* webpackChunkName: "auth" */ '../views/Auth/index.vue'),
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+  
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history:createWebHashHistory(),
   routes,
 });
 
