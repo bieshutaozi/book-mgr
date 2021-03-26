ff7927e491232f5c92774ff7fbe41d6420550907
bieshutaozi <1070048332@qq.com>
2021-03-26 13:56:01 +0800

    finish

--- book-mgr-fe/src/router/index.js
+++ book-mgr-fe/src/router/index.js
@@ -2,11 +2,15 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
   {
-    path: '/auth',
-    name: 'Auth',
-    component: () => import(/* webpackChunkName: "auth" */ '../views/Auth/index.vue'),
+    path: "/auth",
+    name: "Auth",
+    component: () => import(/* webpackChunkName: "auth" */ "../views/Auth/index.vue")
   },
-  
+  {
+    path: "/",
+    name: "BasicLayout",
+    component: () => import(/* webpackChunkName: "auth" */ "../layout/BasicLayout/index.vue")
+  }
 ];
 
 const router = createRouter({
